635c5dc31890ef4ae364d59e659b03955a341d15
Hiren Nagaria <hirennagaria@yahoo.co.in>
2020-06-03 22:55:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const search = instantsearch({
-  appId: 'latency',
-  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
-  indexName: 'movies' });
+  appId: 'QVJBA6E0CO',
+  apiKey: '5f0740f24e581bfadd3058de42ed4c68',
+  indexName: 'dev_pokemon_char' });
 
 
 search.addWidget(
